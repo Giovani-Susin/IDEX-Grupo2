b78871cc931919a3f787dbff745d3a893962f544
Mateus Schenato <145774625+mateusfkschenato@users.noreply.github.com>
2026-04-12 22:43:06 -0300

    Começando a fazer o Footer
    .

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -1,2 +1,9 @@
-<div>
-Ola mundo
+export default funcion Footer () {
+  return (
+  <div>
+    IDE © Todos os direitos reservados.
+    < br/>
+    Termos de Uso | Politica de Privacidade | Política de Cookies
+  </div>
+    );
+}
